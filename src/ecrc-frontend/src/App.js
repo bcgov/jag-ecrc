2479a979c6ec91740197a956c63efb180c4935b1
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-07 11:13:50 -0800

    Added bcgov bootstrap and bootstrap packages, added copy-paste of bcgov bootstrap header and footer components, updated header footer for react

--- src/ecrc-frontend/src/App.js
+++ src/ecrc-frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+
 export default function App() {
   return (
     <div>
+      <Header />
       <h1>eCRC Frontend</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/ecrc-frontend/src/components/header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Header(props) {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container">
+          <a className="navbar-brand" href="https://www2.gov.bc.ca">
+            <img
+              className="img-fluid d-none d-md-block"
+              src="/images/bcid-logo-rev-en.svg"
+              width="181"
+              height="44"
+              alt="B.C. Government Logo"
+            />
+            <img
+              className="img-fluid d-md-none"
+              src="/images/bcid-symbol-rev.svg"
+              width="64"
+              height="44"
+              alt="B.C. Government Logo"
+            />
+          </a>
+          <div className="navbar-brand">{props.name}</div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="/">
+                Home
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
