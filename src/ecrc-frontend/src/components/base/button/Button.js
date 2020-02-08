4bb26c37ff32ae190da948097f062dc9c0aac405
PeggyZhangNtt <190728@nttdata.com>
2020-02-07 16:08:37 -0800

    update-button

--- src/ecrc-frontend/src/components/base/button/Button.js
+++ src/ecrc-frontend/src/components/base/button/Button.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Button(props) {
   return (
-    <button className="btn btn-default" onClick={this.props.handleClick}>
-      {this.props.label}
+    <button className="btn btn-default" onClick="{props.handleClick}">
+      {props.label}
     </button>
   );
 }
