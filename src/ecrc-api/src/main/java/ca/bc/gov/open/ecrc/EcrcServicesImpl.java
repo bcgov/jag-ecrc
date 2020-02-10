dd46d9015d130bdd2b5c8f4c1dca97a2e4fb16cb
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-02-10 14:12:39 -0800

    get province list controller and env properties

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/EcrcServicesImpl.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/EcrcServicesImpl.java
@@ -1,22 +1,56 @@
 package ca.bc.gov.open.ecrc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ca.bc.gov.open.ecrc.exception.EcrcServiceException;
+import ca.bc.gov.open.ecrc.objects.GetProvinceList;
+import reactor.core.publisher.Mono;
 
 /**
  * 
- * Service Implementation class. 
+ * Service Implementation class.
  * 
  * @author shaunmillargov
  *
  */
 @Service
 public class EcrcServicesImpl implements EcrcServices {
-	
-	public String doAuthenticateUser(String accessCode) throws EcrcServiceException {
-		//TODO - code to be added here and change response object. 
-		return null; 
-	}
-	
-}
 
+	private final WebClient webClient;
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Value("${rest.getProvincesList}")
+	private String provinceListUri;
+
+	public EcrcServicesImpl(@Value("${rest.baseUrl}") final String baseUrl,
+			@Value("${rest.userName}") final String userName, @Value("${rest.password}") final String password) {
+		this.webClient = WebClient.builder().baseUrl(baseUrl)
+				.defaultHeaders(header -> header.setBasicAuth(userName, password)).build();
+	}
+
+	public String doAuthenticateUser(String accessCode) throws EcrcServiceException {
+		// TODO - code to be added here and change response object.
+		return null;
+	}
+
+	public String getProvinceList() throws EcrcServiceException {
+		Mono<GetProvinceList> responseBody = this.webClient.get().uri(provinceListUri).retrieve()
+				.bodyToMono(GetProvinceList.class);
+		String response;
+		try {
+			response = objectMapper.writeValueAsString(responseBody.block());
+		} catch (JsonProcessingException e) {
+			throw new EcrcServiceException("JsonProcessingException");
+		}
+		return response;
+	}
+
+}
---
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/GetProvinceListController.java
@@ -0,0 +1,26 @@
+package ca.bc.gov.open.ecrc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.bc.gov.open.ecrc.EcrcServices;
+import ca.bc.gov.open.ecrc.exception.EcrcServiceException;
+
+/**
+ * @author sivakaruna
+ *
+ */
+@RestController
+public class GetProvinceListController {
+	
+	@Autowired 
+	EcrcServices ecrcServices;
+
+	@CrossOrigin(origins = "/**")
+	@GetMapping("/getProvinceList")
+	public String getProvinceList() throws EcrcServiceException {
+		return ecrcServices.getProvinceList();
+	}
+}
