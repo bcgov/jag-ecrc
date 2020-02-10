dbf2dcfd8ec6a16c012de1934e87faf88d5c2353
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-10 11:22:44 -0800

    Removed reference to header/footer from App.js

--- src/ecrc-frontend/src/App.js
+++ src/ecrc-frontend/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
 
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-
 export default function App() {
   return (
     <div>
-      <Header />
       <h1>eCRC Frontend</h1>
-      <Footer />
     </div>
   );
 }
