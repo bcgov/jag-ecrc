762179b8e6a4ad70ad34d7a9753ce18f993354ae
PeggyZhangNtt <190728@nttdata.com>
2020-02-10 10:24:55 -0800

    add button.js and stories for it.

--- src/ecrc-frontend/src/components/base/button/Button.js
+++ src/ecrc-frontend/src/components/base/button/Button.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./Button.css";
 
-export default function Button(props) {
+const STYLES = ["btn--primary--solid", "btn--warning--solid"];
+
+const SIZES = ["btn--medium", "btn--small"];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
   return (
-    <button className="btn btn-default" onClick="{props.handleClick}">
-      {props.label}
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      type={type}
+      onClick={onClick}
+    >
+      {children}
     </button>
   );
-}
+};
---
+++ src/ecrc-frontend/src/components/base/button/Button.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import { Button } from "./Button";
+
+export default {
+  component: Button,
+  title: "Button",
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/
+};
+
+export const buttonData = {
+  children: "buttonChildren",
+  type: "submit",
+  onClick: action("onButtonClicked"),
+  buttonStyle: "btn--primary--solid",
+  buttonSize: "btn--medium"
+};
+
+export const Default = () => {
+  return <Button {...buttonData} />;
+};
