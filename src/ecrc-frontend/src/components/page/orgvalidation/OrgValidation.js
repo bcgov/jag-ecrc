85f996cf9113210db72288498122244254e2d468
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-11 14:11:53 -0800

    Created orgvalidation page template

---
+++ src/ecrc-frontend/src/components/page/orgvalidation/OrgValidation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../base/header/Header";
+import Footer from "../../base/footer/Footer";
+
+export default function OrgValidation() {
+  return (
+    <main>
+      <Header header={header} />
+      <Footer />
+    </main>
+  );
+}
