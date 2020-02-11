09af74a832a7de20dc5d1c30c3357aa4ebaafd87
PeggyZhangNtt <190728@nttdata.com>
2020-02-11 13:56:16 -0800

    text input

--- src/ecrc-frontend/src/components/base/textInput/TextInput.js
+++ src/ecrc-frontend/src/components/base/textInput/TextInput.js
@@ -2,13 +2,61 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./TextInput.css";
 
-export const TextInput = ({ textInput: { placeHolder } }) => {
-  return <input type="text" name={placeHolder} />;
+export const TextInput = ({
+  textInput: { label, id, textInputStyle, value }
+}) => {
+  let textStyle = "textinput_editable_white";
+  const validStyles = [
+    "textinput_editable_gray",
+    "textinput_non_editable_gray",
+    "textinput_editable_white"
+  ];
+
+  if (validStyles.includes(textInputStyle)) {
+    textStyle = textInputStyle;
+  }
+
+  if (textStyle === "textinput_non_editable_gray")
+    return (
+      <div>
+        <div>
+          <label className="textinput_label" htmlFor={id}>
+            {label}
+          </label>
+        </div>
+        <input
+          className={`${textStyle}`}
+          type="text"
+          id={id}
+          value={value}
+          readOnly
+        />
+      </div>
+    );
+
+  return (
+    <div>
+      <div>
+        <label className="textinput_label" htmlFor={id}>
+          {label}
+        </label>
+      </div>
+      <input
+        className={`${textStyle}`}
+        type="text"
+        id={id}
+        defaultValue={value}
+      />
+    </div>
+  );
 };
 
 TextInput.propTypes = {
   textInput: PropTypes.shape({
-    placeHolder: PropTypes.string.isRequired
+    label: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    textInputStyle: PropTypes.string,
+    value: PropTypes.string
   }).isRequired
 };
 
---
+++ src/ecrc-frontend/src/components/base/textInput/TextInput.stories.js
@@ -0,0 +1,40 @@
+import React from "react";
+// import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import { TextInput } from "./TextInput";
+
+const textInput = {
+  label: "button label",
+  id: "textInputId",
+  textInputStyle: "placeHolder"
+};
+
+storiesOf("TextInput", module)
+  .add("Default", () => <TextInput textInput={textInput} />)
+  .add("non-editable-gray", () => (
+    <TextInput
+      textInput={{
+        ...textInput,
+        textInputStyle: "textinput_non_editable_gray",
+        value: "initial value"
+      }}
+    />
+  ))
+  .add("editable-white", () => (
+    <TextInput
+      textInput={{
+        ...textInput,
+        textInputStyle: "textinput_editable_white",
+        value: "initial value"
+      }}
+    />
+  ))
+  .add("editable-gray", () => (
+    <TextInput
+      textInput={{
+        ...textInput,
+        textInputStyle: "textinput_editable_gray",
+        value: "initial value"
+      }}
+    />
+  ));
