281dc1d0957606dee4485ed90a1754a5ed748f9c
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-11 12:17:45 -0800

    Added footer stories and tests

---
+++ src/ecrc-frontend/src/components/base/footer/Footer.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Header from "./Footer";
+import Footer from "./Footer";
+
+export default {
+  title: "Footer",
+  component: Footer
+};
+
+export const Default = () => <Footer />;
