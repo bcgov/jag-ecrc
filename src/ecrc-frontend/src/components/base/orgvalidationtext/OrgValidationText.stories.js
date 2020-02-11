5a5703d5f45150bacb3b74cf885071bab7bc0beb
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-11 14:29:32 -0800

    Added org validation text

---
+++ src/ecrc-frontend/src/components/base/orgvalidationtext/OrgValidationText.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import OrgValidationText from "./OrgValidationText";
+
+export default {
+  title: "OrgValidationText",
+  component: OrgValidationText
+};
+
+export const Default = () => <OrgValidationText />;
--- src/ecrc-frontend/src/components/page/orgvalidation/OrgValidation.js
+++ src/ecrc-frontend/src/components/page/orgvalidation/OrgValidation.js
@@ -3,6 +3,10 @@ import Header from "../../base/header/Header";
 import Footer from "../../base/footer/Footer";
 
 export default function OrgValidation() {
+  const header = {
+    name: "eCRC"
+  };
+
   return (
     <main>
       <Header header={header} />
