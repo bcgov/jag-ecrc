8c96345a9477005f1f25639cbb05c6a14dfc38ca
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-02-11 12:46:00 -0800

    Refactored province list controller to conform to pattern - 164

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/EcrcServicesImpl.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/EcrcServicesImpl.java
@@ -1,13 +1,11 @@
 package ca.bc.gov.open.ecrc;
 
 import ca.bc.gov.open.ecrc.objects.DoAuthenticateUser;
-import javassist.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -48,9 +46,8 @@ public class EcrcServicesImpl implements EcrcServices {
 		doAuthenticateUserUri = String.format(doAuthenticateUserUri, accessCode);
 		Mono<DoAuthenticateUser> responseBody = this.webClient.get().uri(doAuthenticateUserUri).retrieve()
 				.bodyToMono(DoAuthenticateUser.class);
-		String response;
 		try {
-			if (responseBody.block().getResponseCode() == 1) {
+			if (responseBody.block().getResponseCode() == 0) {
 				return objectMapper.writeValueAsString(responseBody.block());
 			} else {
 				return null;
@@ -67,16 +64,18 @@ public class EcrcServicesImpl implements EcrcServices {
 	public String getProvinceList() throws EcrcServiceException {
 		Mono<GetProvinceList> responseBody = this.webClient.get().uri(provinceListUri).retrieve()
 				.bodyToMono(GetProvinceList.class);
-		String response;
 		try {
-			response = objectMapper.writeValueAsString(responseBody.block());
+			if (responseBody.block().getResponseCode() == 0) {
+				return objectMapper.writeValueAsString(responseBody.block());
+			} else {
+				return null;
+			}
 		} catch (JsonProcessingException e) {
-			// TODO - Log exception
+			logger.error("Failed to convert to json processing exception");
 			throw new EcrcServiceException(EcrcExceptionConstants.CONVERT_TO_JSON_ERROR, e);
 		} catch (Exception e) {
-			// TODO - Log exception
+			logger.error("Failed to convert to json general exception");
 			throw new EcrcServiceException(EcrcExceptionConstants.WEBSERVICE_RESPONSE_ERROR, e);
 		}
-		return response;
 	}
 }
