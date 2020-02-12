000dd0da90a0b753a9e9088119a64d005a76a0c7
TayGov <Taylor.Clausen@gov.bc.ca>
2020-02-12 08:30:13 -0800

    Updated test to run with dependency on env variables

--- src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/EcrcServicesImplTest.java
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/EcrcServicesImplTest.java
@@ -1,11 +1,29 @@
 package ca.bc.gov.open.ecrc;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 public class EcrcServicesImplTest {
-    @Test
-    public void testResult() {
+    @InjectMocks
+    EcrcServicesImpl ecrcServices;
 
+    @Mock
+    WebClient webClient;
+
+    @BeforeEach
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void testDoAuthenticateResult() {
+        
     }
 }
--- src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/controller/DoAuthenticateUserControllerTest.java
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/controller/DoAuthenticateUserControllerTest.java
@@ -1,15 +1,38 @@
 package ca.bc.gov.open.ecrc.controller;
 
+import ca.bc.gov.open.ecrc.EcrcServicesImpl;
+import ca.bc.gov.open.ecrc.exception.EcrcServiceException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
-import ca.bc.gov.open.ecrc.controller.DoAuthenticateUserController;
 
 public class DoAuthenticateUserControllerTest {
-	@Test
-	public void testOddValidOrg() {
+	@InjectMocks
+	DoAuthenticateUserController doAuthenticateUserController;
 
+	@Mock
+	EcrcServicesImpl ecrcServices;
+
+	@Mock
+	ObjectMapper objectMapper;
+
+	@BeforeEach
+	public void initMocks(){
+		MockitoAnnotations.initMocks(this);
 	}
+
+	@Test
+	public void testOddValidOrg() throws EcrcServiceException {
+		Mockito.when(ecrcServices.doAuthenticateUser("SOMEDATA")).thenReturn("SOMESTRING");
+		ResponseEntity<String> result = doAuthenticateUserController.doAuthenticateUser("SOMEDATA");
+		Assertions.assertEquals("SOMESTRING", result.getBody());
+	}
+
 }
