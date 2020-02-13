938200c64527dae8a1a9dbf99d73cf8e8edd70c5
TayGov <Taylor.Clausen@gov.bc.ca>
2020-02-13 09:12:39 -0800

    Add object for CRC Service

---
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/model/CRCService.java
@@ -0,0 +1,139 @@
+package ca.bc.gov.open.ecrc.model;
+
+public class CRCService {
+    private String orgTicketNumber;
+    private String schedule_Type_Cd;
+    private String scope_Level_Cd;
+    private String appl_Party_Id;
+    private String org_Appl_To_Pay;
+    private String applicant_Posn;
+    private String child_Care_Fac_Nm;
+    private String governing_Body_Nm;
+    private String session_Id;
+    private String invoice_Id;
+    private String auth_Release_EIV_Vendor_YN;
+    private String auth_Conduct_CRC_Check_YN;
+    private String auth_Release_To_Org_YN;
+    private String appl_Identity_Verified_EIV_YN;
+    private String eivPassDetailsResults;
+
+    public String getOrgTicketNumber() {
+        return orgTicketNumber;
+    }
+
+    public void setOrgTicketNumber(String orgTicketNumber) {
+        this.orgTicketNumber = orgTicketNumber;
+    }
+
+    public String getSchedule_Type_Cd() {
+        return schedule_Type_Cd;
+    }
+
+    public void setSchedule_Type_Cd(String schedule_Type_Cd) {
+        this.schedule_Type_Cd = schedule_Type_Cd;
+    }
+
+    public String getScope_Level_Cd() {
+        return scope_Level_Cd;
+    }
+
+    public void setScope_Level_Cd(String scope_Level_Cd) {
+        this.scope_Level_Cd = scope_Level_Cd;
+    }
+
+    public String getAppl_Party_Id() {
+        return appl_Party_Id;
+    }
+
+    public void setAppl_Party_Id(String appl_Party_Id) {
+        this.appl_Party_Id = appl_Party_Id;
+    }
+
+    public String getOrg_Appl_To_Pay() {
+        return org_Appl_To_Pay;
+    }
+
+    public void setOrg_Appl_To_Pay(String org_Appl_To_Pay) {
+        this.org_Appl_To_Pay = org_Appl_To_Pay;
+    }
+
+    public String getApplicant_Posn() {
+        return applicant_Posn;
+    }
+
+    public void setApplicant_Posn(String applicant_Posn) {
+        this.applicant_Posn = applicant_Posn;
+    }
+
+    public String getChild_Care_Fac_Nm() {
+        return child_Care_Fac_Nm;
+    }
+
+    public void setChild_Care_Fac_Nm(String child_Care_Fac_Nm) {
+        this.child_Care_Fac_Nm = child_Care_Fac_Nm;
+    }
+
+    public String getGoverning_Body_Nm() {
+        return governing_Body_Nm;
+    }
+
+    public void setGoverning_Body_Nm(String governing_Body_Nm) {
+        this.governing_Body_Nm = governing_Body_Nm;
+    }
+
+    public String getSession_Id() {
+        return session_Id;
+    }
+
+    public void setSession_Id(String session_Id) {
+        this.session_Id = session_Id;
+    }
+
+    public String getInvoice_Id() {
+        return invoice_Id;
+    }
+
+    public void setInvoice_Id(String invoice_Id) {
+        this.invoice_Id = invoice_Id;
+    }
+
+    public String getAuth_Release_EIV_Vendor_YN() {
+        return auth_Release_EIV_Vendor_YN;
+    }
+
+    public void setAuth_Release_EIV_Vendor_YN(String auth_Release_EIV_Vendor_YN) {
+        this.auth_Release_EIV_Vendor_YN = auth_Release_EIV_Vendor_YN;
+    }
+
+    public String getAuth_Conduct_CRC_Check_YN() {
+        return auth_Conduct_CRC_Check_YN;
+    }
+
+    public void setAuth_Conduct_CRC_Check_YN(String auth_Conduct_CRC_Check_YN) {
+        this.auth_Conduct_CRC_Check_YN = auth_Conduct_CRC_Check_YN;
+    }
+
+    public String getAuth_Release_To_Org_YN() {
+        return auth_Release_To_Org_YN;
+    }
+
+    public void setAuth_Release_To_Org_YN(String auth_Release_To_Org_YN) {
+        this.auth_Release_To_Org_YN = auth_Release_To_Org_YN;
+    }
+
+    public String getAppl_Identity_Verified_EIV_YN() {
+        return appl_Identity_Verified_EIV_YN;
+    }
+
+    public void setAppl_Identity_Verified_EIV_YN(String appl_Identity_Verified_EIV_YN) {
+        this.appl_Identity_Verified_EIV_YN = appl_Identity_Verified_EIV_YN;
+    }
+
+    public String getEivPassDetailsResults() {
+        return eivPassDetailsResults;
+    }
+
+    public void setEivPassDetailsResults(String eivPassDetailsResults) {
+        this.eivPassDetailsResults = eivPassDetailsResults;
+    }
+}
