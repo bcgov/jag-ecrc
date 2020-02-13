e0013d4f9150e98c55ce2392d977726f639e81a1
PeggyZhangNtt <190728@nttdata.com>
2020-02-13 12:02:42 -0800

    complete form story

--- src/ecrc-frontend/src/components/composite/form/Form.js
+++ src/ecrc-frontend/src/components/composite/form/Form.js
@@ -4,25 +4,34 @@ import "./Form.css";
 import { TextInput } from "../../base/textInput/TextInput";
 import { Button } from "../../base/button/Button";
 
-export const Form = ({ textInputs, buttons }) => {
+export const Form = ({ title, textInputs, buttons }) => {
   const inputList = textInputs.map(input => {
     return (
-      <li>
-        <TextInput key={input.name} textInput={input} />
+      <li key={input.name}>
+        <TextInput textInput={input} onChange={input.onChange} />
       </li>
     );
   });
 
   const buttonList = buttons.map(button => {
     return (
-      <li>
-        <Button key={button.name} button={button} />
+      <li key={button.name}>
+        <Button
+          button={{
+            label: button.label,
+            buttonStyle: button.buttonStyle,
+            buttonSize: button.buttonSize,
+            type: button.type
+          }}
+          onClick={button.onClick}
+        />
       </li>
     );
   });
 
   return (
     <form className="ecrcForm">
+      {title != null && <div className="ecrcForm_title">{title}</div>}
       {inputList.length > 0 && <ul>{inputList}</ul>}
       {buttonList.length > 0 && <ul id="buttonList">{buttonList}</ul>}
     </form>
@@ -30,10 +39,15 @@ export const Form = ({ textInputs, buttons }) => {
 };
 
 Form.propTypes = {
+  title: PropTypes.string,
   textInputs: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired
+      label: PropTypes.string.isRequired,
+      textInputStyle: PropTypes.string,
+      value: PropTypes.string,
+      onChange: PropTypes.func,
+      isRequired: PropTypes.bool
     })
   ),
   buttons: PropTypes.arrayOf(
@@ -41,12 +55,14 @@ Form.propTypes = {
       label: PropTypes.string.isRequired,
       buttonStyle: PropTypes.string.isRequired,
       buttonSize: PropTypes.string.isRequired,
-      type: PropTypes.string.isRequired
+      type: PropTypes.string.isRequired,
+      onClick: PropTypes.func
     })
   )
 };
 
 Form.defaultProps = {
+  title: null,
   textInputs: [],
   buttons: []
 };
--- src/ecrc-frontend/src/components/composite/form/Form.stories.js
+++ src/ecrc-frontend/src/components/composite/form/Form.stories.js
@@ -8,15 +8,21 @@ import { Form } from "./Form";
 const textInputs = [
   {
     label: "label1",
-    id: "textInput1"
+    id: "textInput1",
+    onChange: action("onInputText1_Changed")
   },
   {
     label: "label2",
-    id: "textInput2"
+    id: "textInput2",
+    value: "initValue",
+    textInputStyle: "textinput_non_editable_gray",
+    onChange: action("onInputText2_Changed")
   },
   {
     label: "label3",
-    id: "textInput3"
+    id: "textInput3",
+    textInputStyle: "textinput_editable_gray",
+    isRequired: true
   }
 ];
 
@@ -25,18 +31,30 @@ const buttons = [
     label: "button1",
     buttonStyle: "btn btn-primary",
     buttonSize: "btn btn-sm",
-    type: "submit",
-    onClick: action("onButtonClicked")
+    type: "reset",
+    onClick: action("onButton1Clicked")
   },
   {
     label: "button2",
     buttonStyle: "btn btn-warning",
     buttonSize: "btn btn-lg",
     type: "button",
-    onClick: action("onButtonClicked")
+    onClick: action("onButton2Clicked")
+  },
+  {
+    label: "button3",
+    buttonStyle: "btn btn-danger",
+    buttonSize: "btn btn-sm",
+    type: "button",
+    onClick: action("onButton3Clicked")
   }
 ];
 
-storiesOf("Form", module).add("Default", () => (
-  <Form textInputs={textInputs} buttons={buttons} />
-));
+storiesOf("Form", module)
+  .add("Default", () => <Form />)
+  .add("3texts_3buttons", () => (
+    <Form textInputs={textInputs} buttons={buttons} />
+  ))
+  .add("Title_3texts_3buttons", () => (
+    <Form title="title" textInputs={textInputs} buttons={buttons} />
+  ));
