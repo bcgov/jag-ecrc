c405518c4d1a10c23d2040c8ee06c4e4a5586957
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-02-14 14:30:18 -0800

    add base component

---
+++ src/ecrc-frontend/src/components/base/termsOfUse/TermsOfUse.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import TermsOfUse from "./TermsOfUse";
+
+export default {
+  title: "Terms of Use",
+  component: TermsOfUse
+};
+
+export const Default = () => <TermsOfUse />;
