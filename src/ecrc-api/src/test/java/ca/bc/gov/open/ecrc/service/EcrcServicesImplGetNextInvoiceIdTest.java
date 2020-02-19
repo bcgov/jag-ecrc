c21f117f13669afe8b3e44cee0c374a32e15a876
TayGov <Taylor.Clausen@gov.bc.ca>
2020-02-19 08:29:22 -0800

    Implement Get Next Invoice Service

---
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/service/EcrcServicesImplGetNextInvoiceIdTest.java
@@ -0,0 +1,44 @@
+package ca.bc.gov.open.ecrc.service;
+
+import ca.bc.gov.open.ecrc.configuration.EcrcProperties;
+import ca.bc.gov.open.ecrc.exception.EcrcServiceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.mockito.ArgumentMatchers.any;
+
+public class EcrcServicesImplGetNextInvoiceIdTest {
+    private ResponseEntity<String> serviceResult;
+    private String result = "{\"sessionId\":5742,\"message\":\"Success\",\"responseCode\":0}";
+    @InjectMocks
+    EcrcServicesImpl ecrcServices;
+
+    @Mock
+    EcrcWebMethodsService ecrcWebMethodsService;
+
+    @Mock
+    EcrcProperties ecrcProperties;
+
+    @BeforeEach
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        Mockito.when(ecrcProperties.getGetNextInvoiceIdUri()).thenReturn("invoiceurl?%s");
+    }
+
+    @DisplayName("Success - ecrcService get NextInvoiceId")
+    @Test
+    public void testGetNextInvoiceIdSuccess() throws EcrcServiceException {
+        Mockito.when(ecrcWebMethodsService.callWebMethodsService(any(), any())).thenReturn(new ResponseEntity<>(result, HttpStatus.OK));
+        serviceResult = ecrcServices.getNextInvoiceId("CRCE");
+        Assertions.assertEquals(HttpStatus.OK, serviceResult.getStatusCode());
+        Assertions.assertEquals(result, serviceResult.getBody());
+    }
+}
