a0b8686dd5c80bcbd2e2b23884c8624de73032e4
Alan Dodge <alan.dodge@gov.bc.ca>
2020-02-19 13:51:54 -0800

    Improved styling for access code side card

--- src/ecrc-frontend/src/components/composite/accesscodesidecard/AccessCodeSideCard.js
+++ src/ecrc-frontend/src/components/composite/accesscodesidecard/AccessCodeSideCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./AccessCodeSideCard.css";
 import SideCard from "../../base/sideCard/SideCard";
 
 export default function AccessCodeSideCard() {
@@ -7,7 +8,7 @@ export default function AccessCodeSideCard() {
     heading: "Need help with an access code?",
     content: [
       "Access code is specific to each organization. If you need help with the organization access code, please ",
-      <b>contact your organization.</b>
+      <b className="block">contact your organization.</b>
     ],
     type: "bluegrey"
   };
