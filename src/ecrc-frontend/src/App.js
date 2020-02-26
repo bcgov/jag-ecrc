8158a9a98303219234a5aaafc8177794ceb70b3f
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-02-26 13:03:52 -0800

    add basic page layout and content

--- src/ecrc-frontend/src/App.js
+++ src/ecrc-frontend/src/App.js
@@ -7,6 +7,7 @@ import Transition from "./components/page/transition/Transition";
 import TOU from "./components/page/tou/TOU";
 import Consent from "./components/page/consent/Consent";
 import BcscRedirect from "./components/page/bcscRedirect/BcscRedirect";
+import UserConfirmation from "./components/page/userConfirmation/UserConfirmation";
 
 export default function App() {
   const [org, setOrg] = useState(
@@ -45,6 +46,9 @@ export default function App() {
           </Route>
           <Route path="/ecrc/bcscRedirect">
             <BcscRedirect page={{ header }} />
+          </Route>
+          <Route path="/ecrc/userconfirmation">
+            <UserConfirmation header={header} />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/ecrc-frontend/src/components/page/userConfirmation/UserConfirmation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Header from "../../base/header/Header";
+import Footer from "../../base/footer/Footer";
+import { Button } from "../../base/button/Button";
+import "./UserConfirmation.css";
+
+export default function UserConfirmation({ header }) {
+  const yesButton = {
+    label: "Yes",
+    buttonStyle: "btn btn-primary",
+    buttonSize: "btn",
+    type: "submit"
+  };
+
+  const noButton = {
+    label: "No",
+    buttonStyle: "btn btn-primary",
+    buttonSize: "btn",
+    type: "submit"
+  };
+
+  return (
+    <main>
+      <Header header={header} />
+      <div className="page">
+        <div className="user-confirm-content col-md-12">
+          <strong>
+            Please confirm the name associated with the BC Service card login
+            provided.
+          </strong>
+          <p />
+          <p>Temp name</p>
+          <p>Is this correct?</p>
+          <div className="row">
+            <div className="col-md-12">
+              <Button button={yesButton} onClick={() => {}} />
+              <Button button={noButton} onClick={() => {}} />
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer isSmallPage />
+    </main>
+  );
+}
+
+UserConfirmation.propTypes = {
+  header: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired
+};
