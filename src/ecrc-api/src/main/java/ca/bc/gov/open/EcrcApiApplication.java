9852e14ab9b934152283949888321e082b27fe60
TayGov <Taylor.Clausen@gov.bc.ca>
2020-02-28 07:30:06 -0800

    Removed unused imports

--- src/ecrc-api/src/main/java/ca/bc/gov/open/EcrcApiApplication.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/EcrcApiApplication.java
@@ -1,15 +1,7 @@
 package ca.bc.gov.open;
 
-import ca.bc.gov.open.ecrc.security.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
 @SpringBootApplication
 public class EcrcApiApplication {
