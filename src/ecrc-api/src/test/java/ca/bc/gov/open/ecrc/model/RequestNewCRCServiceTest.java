8f36e12d894226236976e2bda774251d52f0329f
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-02-28 15:51:22 -0800

    Updated junit test

--- src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/model/RequestNewCRCServiceTest.java
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/model/RequestNewCRCServiceTest.java
@@ -4,22 +4,22 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class RequestNewCRCServiceTest {
-    private final String expectedQueryString = "? " +
+    private final String expectedQueryString = "?" +
             "OrgTicketNumber=orgTicketNumber" +
-            "& Schedule_Type_Cd=schedule_Type_Cd" +
-            "& Scope_Level_Cd=scope_Level_Cd" +
-            "& Appl_Party_Id=appl_Party_Id" +
-            "& Org_Appl_To_Pay=org_Appl_To_Pay" +
-            "& Applicant_Posn=applicant_Posn" +
-            "& Child_Care_Fac_Nm=child_Care_Fac_Nm" +
-            "& Governing_Body_Nm=governing_Body_Nm" +
-            "& Session_Id=session_Id" +
-            "& Invoice_Id=invoice_Id" +
-            "& Auth_Release_EIV_Vendor_YN=auth_Release_EIV_Vendor_YN" +
-            "& Auth_Conduct_CRC_Check_YN=auth_Conduct_CRC_Check_YN" +
-            "& Auth_Release_To_Org_YN=auth_Release_To_Org_YN" +
-            "& Appl_Identity_Verified_EIV_YN=appl_Identity_Verified_EIV_YN" +
-            "& EivPassDetailsResults=eivPassDetailsResults";
+            "&Schedule_Type_Cd=schedule_Type_Cd" +
+            "&Scope_Level_Cd=scope_Level_Cd" +
+            "&Appl_Party_Id=appl_Party_Id" +
+            "&Org_Appl_To_Pay=org_Appl_To_Pay" +
+            "&Applicant_Posn=applicant_Posn" +
+            "&Child_Care_Fac_Nm=child_Care_Fac_Nm" +
+            "&Governing_Body_Nm=governing_Body_Nm" +
+            "&Session_Id=session_Id" +
+            "&Invoice_Id=invoice_Id" +
+            "&Auth_Release_EIV_Vendor_YN=auth_Release_EIV_Vendor_YN" +
+            "&Auth_Conduct_CRC_Check_YN=auth_Conduct_CRC_Check_YN" +
+            "&Auth_Release_To_Org_YN=auth_Release_To_Org_YN" +
+            "&Appl_Identity_Verified_EIV_YN=appl_Identity_Verified_EIV_YN" +
+            "&EivPassDetailsResults=eivPassDetailsResults";
     @Test
     public void generateQueryStringTest() {
         RequestNewCRCService requestNewCRCService = new RequestNewCRCService();
