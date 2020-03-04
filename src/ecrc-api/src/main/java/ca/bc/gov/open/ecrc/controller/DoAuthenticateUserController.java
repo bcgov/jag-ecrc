40dca838b91e6f0090170b62eed7938dca4f9232
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-03 16:24:42 -0800

    remove unused changes

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/DoAuthenticateUserController.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/DoAuthenticateUserController.java
@@ -12,12 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
-
 import java.util.UUID;
 
 /**
