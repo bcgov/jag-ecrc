4e9d793a28e16ea979dc0a782ada05a2e78df2ee
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-04 11:21:38 -0800

    Refactor BC bootstrap import for OpenShift

--- src/ecrc-frontend/src/index.js
+++ src/ecrc-frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import "@bcgov/bootstrap-theme/dist/css/bootstrap-theme.min.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
