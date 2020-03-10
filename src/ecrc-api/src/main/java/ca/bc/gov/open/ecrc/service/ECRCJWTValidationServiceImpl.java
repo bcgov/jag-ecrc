693c163c7e6b00f0ff4e9ccdd5b6415fe8bcbca8
TayGov <Taylor.Clausen@gov.bc.ca>
2020-03-10 08:28:48 -0700

    Adding stack to log output.

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/service/ECRCJWTValidationServiceImpl.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/service/ECRCJWTValidationServiceImpl.java
@@ -107,9 +107,8 @@ public class ECRCJWTValidationServiceImpl implements ECRCJWTValidationService {
 		try {
 			keySource = new RemoteJWKSet<>(new URL(oidcConfigService.getConfig().getJwksUri()));
 		} catch (MalformedURLException e1) {
-			logger.error("Unable to instantiate Remote JWK set from remote server. " + e1.getMessage());
+			logger.error("Unable to instantiate Remote JWK set from remote server.", e1);
 			val.setMessage(e1.getMessage());
-			e1.printStackTrace();
 		}
 
 		// The expected JWS algorithm of the id_Token received from BCSC
@@ -134,10 +133,9 @@ public class ECRCJWTValidationServiceImpl implements ECRCJWTValidationService {
 			jwtProcessor.process(token, ctx);
 			logger.debug("Token Ok");
 		} catch (ParseException | BadJOSEException | JOSEException e) {
-			logger.error("Token FAILED validation. " + e.getMessage(), e);
+			logger.error("Token FAILED validation.", e);
 			val.setMessage(e.getMessage());
 			val.setValid(false);
-			e.printStackTrace();
 		}
 
 		return val;
@@ -161,9 +159,9 @@ public class ECRCJWTValidationServiceImpl implements ECRCJWTValidationService {
 			obj = (JSONObject) p.parse(_tokens);
 			response = TokenResponse.parse(obj);
 		} catch (net.minidev.json.parser.ParseException e) {
-			e.printStackTrace();
+			logger.error("PER Vaildate Failed to Parse. ", e);
 		} catch (com.nimbusds.oauth2.sdk.ParseException e) {
-			e.printStackTrace();
+			logger.error("PER Vaildate Failed to Parse. ", e);
 		}
 		
 		// Fetch both tokens within the decrypted string. 
