74bfdfd67ed4c7c6a1872b411f91d6ebb8f51f51
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-11 16:38:54 -0700

    linting

--- src/ecrc-frontend/src/components/page/consent/Consent.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
