94a1659702b74ead84efde48557255507b4292f4
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-11 08:30:08 -0700

    Removed hardcoded url to redirect back to current host domain

--- src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
+++ src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
@@ -359,9 +359,9 @@ export default function InformationReview({
 
           const createURL = {
             invoiceNumber: invoiceId,
-            approvedPage: "http://localhost:3000/ecrc/success",
-            declinedPage: "http://localhost:3000/ecrc/success",
-            errorPage: "http://localhost:3000/ecrc/success",
+            approvedPage: `${window.location.hostname}/ecrc/success`,
+            declinedPage: `${window.location.hostname}/ecrc/success`,
+            errorPage: `${window.location.hostname}/ecrc/success`,
             totalItemsAmount: serviceFeeAmount,
             serviceIdRef1: serviceId,
             partyIdRef2: partyId
