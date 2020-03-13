4d7bdf0abb85184bdcbaab006c80863bfd80fd96
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-13 12:19:03 -0700

    Removed consolelog

--- src/ecrc-frontend/src/components/page/orgValidation/OrgValidation.js
+++ src/ecrc-frontend/src/components/page/orgValidation/OrgValidation.js
@@ -37,7 +37,6 @@ export default function OrgValidation({ page: { header, setOrg } }) {
         setToOrgVerification(true);
       })
       .catch(error => {
-        console.log("The error is: " + error);
         if (error.response.status === 404) {
           setOrgError("Please enter a valid org code");
         } else if (error.response.status === 401) {
