d12f428e849078748120eea527195aad69f8e415
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-03-16 14:56:14 -0700

    Updated Information Review tests

--- src/ecrc-frontend/src/components/page/informationreview/InformationReview.test.js
+++ src/ecrc-frontend/src/components/page/informationreview/InformationReview.test.js
@@ -1,15 +1,158 @@
 import React from "react";
 import { create } from "react-test-renderer";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Router } from "react-router-dom";
+import {
+  render,
+  fireEvent,
+  getByRole,
+  getByDisplayValue,
+  getByText,
+  wait
+} from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import axios from "axios";
 
 import InformationReview from "./InformationReview";
 
-describe("InformationReview Component", () => {
-  test("Matches the snapshot", () => {
-    const header = {
-      name: "Criminal Record Check"
-    };
+// Mock axios
+jest.mock("axios");
 
+describe("InformationReview Component", () => {
+  const header = {
+    name: "Criminal Record Check"
+  };
+
+  const applicant = {
+    legalFirstNm: "Robert",
+    legalSecondNm: "Norman",
+    legalSurnameNm: "Ross",
+    birthPlace: "Daytona Beach, Florida",
+    alias1FirstNm: "Robert",
+    alias1SecondNm: "Norman",
+    alias1SurnameNm: "Ross",
+    alias2FirstNm: "Robert",
+    alias2SecondNm: "Norman",
+    alias2SurnameNm: "Ross",
+    alias3FirstNm: "Robert",
+    alias3SecondNm: "Norman",
+    alias3SurnameNm: "Ross",
+    birthDt: "1942-10-29",
+    genderTxt: "Male",
+    driversLicNo: "1234567",
+    phoneNumber: "2501234567",
+    emailAddress: "bob.ross@example.com",
+    addressLine1: "123 Somewhere",
+    cityNm: "Here",
+    provinceNm: "British Columbia",
+    postalCodeTxt: "V9V 9V9",
+    countryNm: "Canada",
+    jobTitle: "Painter",
+    organizationFacility: "Something"
+  };
+
+  const org = {
+    orgApplicantRelationship: "EMPLOYEE",
+    orgTicketNumber: "crce",
+    defaultScheduleTypeCd: "WBSD",
+    defaultCrcScopeLevelCd: "WWCH"
+  };
+
+  const setApplicationInfo = jest.fn();
+  const saveApplicant = jest.fn();
+  const saveOrg = jest.fn();
+  const saveApplicationInfo = jest.fn();
+
+  const page = {
+    header,
+    applicant,
+    org,
+    setApplicationInfo,
+    saveApplicant,
+    saveOrg,
+    saveApplicationInfo
+  };
+
+  window.scrollTo = jest.fn();
+
+  // Mock window location
+  const mockWindow = jest.fn();
+  delete window.location;
+  window.location = { assign: mockWindow };
+
+  test("Matches the snapshot", () => {
+    const infoReview = create(
+      <MemoryRouter>
+        <InformationReview page={page} />
+      </MemoryRouter>
+    );
+    expect(infoReview.toJSON()).toMatchSnapshot();
+  });
+
+  test("Validate checkbox", () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <InformationReview page={page} />
+      </Router>
+    );
+
+    expect(getByText(container, "SUBMIT").disabled).toBeTruthy();
+
+    fireEvent.click(getByRole(container, "checkbox"));
+
+    expect(getByText(container, "SUBMIT").disabled).toBeFalsy();
+  });
+
+  test("Validate Back button", () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <InformationReview page={page} />
+      </Router>
+    );
+    fireEvent.click(getByText(container, "EDIT APPLICATION"));
+    expect(history.location.pathname).toEqual("/ecrc/applicationform");
+  });
+
+  test("Validate Employee relationship flow", async () => {
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          sessionId: "123",
+          invoiceId: "123",
+          serviceFeeAmount: "123"
+        }
+      })
+    );
+
+    axios.post.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          partyId: "123",
+          serviceId: "123",
+          urlResponse: "http://sample.com"
+        }
+      })
+    );
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <InformationReview page={page} />
+      </Router>
+    );
+    fireEvent.click(getByRole(container, "checkbox"));
+    fireEvent.click(getByText(container, "SUBMIT"));
+
+    await wait(() => {
+      expect(setApplicationInfo).toHaveBeenCalled();
+      expect(saveApplicant).toHaveBeenCalled();
+      expect(saveOrg).toHaveBeenCalled();
+      expect(saveApplicationInfo).toHaveBeenCalled();
+    });
+  });
+
+  test("Validate Volunteer relationship flow", async () => {
     const applicant = {
       legalFirstNm: "Robert",
       legalSecondNm: "Norman",
@@ -17,7 +160,6 @@ describe("InformationReview Component", () => {
       birthPlace: "Daytona Beach, Florida",
       birthDt: "1942-10-29",
       genderTxt: "Male",
-      driversLicNo: "1234567",
       phoneNumber: "2501234567",
       emailAddress: "bob.ross@example.com",
       addressLine1: "123 Somewhere",
@@ -25,21 +167,15 @@ describe("InformationReview Component", () => {
       provinceNm: "British Columbia",
       postalCodeTxt: "V9V 9V9",
       countryNm: "Canada",
-      jobTitle: "Painter",
-      organizationFacility: ""
+      jobTitle: "Painter"
     };
 
     const org = {
-      orgApplicantRelationship: "EMPLOYEE",
+      orgApplicantRelationship: "VOLUNTEER",
       orgTicketNumber: "crce",
       defaultScheduleTypeCd: "WBSD",
       defaultCrcScopeLevelCd: "WWCH"
     };
-
-    const setApplicationInfo = () => {};
-    const saveApplicant = () => {};
-    const saveOrg = () => {};
-    const saveApplicationInfo = () => {};
 
     const page = {
       header,
@@ -51,11 +187,38 @@ describe("InformationReview Component", () => {
       saveApplicationInfo
     };
 
-    const infoReview = create(
-      <MemoryRouter>
-        <InformationReview page={page} />
-      </MemoryRouter>
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          sessionId: "123",
+          invoiceId: "123",
+          serviceFeeAmount: "123"
+        }
+      })
     );
-    expect(infoReview.toJSON()).toMatchSnapshot();
+
+    axios.post.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          partyId: "123",
+          serviceId: "123",
+          urlResponse: "http://sample.com"
+        }
+      })
+    );
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <InformationReview page={page} />
+      </Router>
+    );
+    fireEvent.click(getByRole(container, "checkbox"));
+    fireEvent.click(getByText(container, "SUBMIT"));
+
+    await wait(() => {
+      expect(setApplicationInfo).toHaveBeenCalled();
+      expect(history.location.pathname).toEqual("/ecrc/success");
+    });
   });
 });
