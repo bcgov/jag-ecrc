2131b2751a598149ebb0bc0a2cf822a0025080e9
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-03-17 18:29:54 -0700

    Updated tests for consent page

--- src/ecrc-frontend/src/components/page/consent/Consent.test.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.test.js
@@ -1,23 +1,90 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import { Router } from "react-router-dom";
+import {
+  render,
+  fireEvent,
+  getAllByRole,
+  getByText
+} from "@testing-library/react";
+import { createMemoryHistory } from "history";
+import { generateJWTToken } from "../../../modules/AuthenticationHelper";
 
 import Consent from "./Consent";
 
 describe("Consent Page Component", () => {
+  window.scrollTo = jest.fn();
+
+  const header = {
+    name: "Criminal Record Check"
+  };
+
+  const page = {
+    header
+  };
+
+  beforeEach(() => {
+    sessionStorage.setItem("validator", "secret");
+    sessionStorage.setItem("uuid", "unique123");
+    generateJWTToken({
+      actionsPerformed: ["userConfirmation"],
+      authorities: ["Authorized"]
+    });
+  });
+
   test("Matches the snapshot", () => {
-    const header = {
-      name: "Criminal Record Check"
-    };
-
-    const page = {
-      header
-    };
-
-    const onContinueClick = () => jest.fn();
-
-    const consent = create(
-      <Consent page={page} onContinueClick={onContinueClick} />
-    );
+    const consent = create(<Consent page={page} />);
     expect(consent.toJSON()).toMatchSnapshot();
+  });
+
+  test("Validate Cancel Redirect", () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Consent page={page} />
+      </Router>
+    );
+
+    fireEvent.click(getByText(container, "Cancel and Exit"));
+    expect(history.location.pathname).toEqual("/hosthome");
+  });
+
+  test("Validate Continue Redirect", () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Consent page={page} />
+      </Router>
+    );
+
+    expect(getByText(container, "Continue").disabled).toBeTruthy();
+
+    fireEvent.click(getAllByRole(container, "checkbox")[0]);
+    fireEvent.click(getAllByRole(container, "checkbox")[1]);
+    fireEvent.click(getAllByRole(container, "checkbox")[2]);
+
+    expect(getByText(container, "Continue").disabled).toBeFalsy();
+
+    fireEvent.click(getByText(container, "Continue"));
+    expect(history.location.pathname).toEqual(
+      "/criminalrecordcheck/applicationform"
+    );
+  });
+
+  test("Validate Redirect to Home when unauthorized", () => {
+    const history = createMemoryHistory();
+
+    generateJWTToken({
+      actionsPerformed: ["none"],
+      authorities: ["Authorized"]
+    });
+
+    render(
+      <Router history={history}>
+        <Consent page={page} />
+      </Router>
+    );
+
+    expect(history.location.pathname).toEqual("/");
   });
 });
