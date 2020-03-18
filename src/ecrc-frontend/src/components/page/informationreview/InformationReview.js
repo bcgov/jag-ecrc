258e730c90781f8cb2308912d5eaff9c5aac252a
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-18 09:55:27 -0700

    Re-added list style to org ver

--- src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
+++ src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
@@ -164,7 +164,7 @@ export default function InformationReview({
     header: (
       <div>
         PREVIOUS NAMES
-        <span className="">
+        <span className="smallTableHeader">
           Including birth name, previous name, maiden name, and alias
         </span>
       </div>
