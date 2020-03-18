7f3c12a866a54756ed1113961b85f589c7d46891
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-18 10:59:36 -0700

    fix tests

--- src/ecrc-frontend/src/setupTests.js
+++ src/ecrc-frontend/src/setupTests.js
@@ -5,6 +5,7 @@
 import "@testing-library/jest-dom/extend-expect";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import "regenerator-runtime/runtime";
 
 configure({ adapter: new Adapter() });
 
