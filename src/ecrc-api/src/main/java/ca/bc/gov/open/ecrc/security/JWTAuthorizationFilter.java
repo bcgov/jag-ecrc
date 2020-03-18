5ff6298d50a8098dcf6659597a887fb79db68630
Shaun Millar <shaun.millar@nttdata.com>
2020-03-17 17:03:09 -0700

    SPDBCSC-454: Completed second half of "per" validation and API access security enhancement.

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/security/JWTAuthorizationFilter.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/security/JWTAuthorizationFilter.java
@@ -9,19 +9,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import ca.bc.gov.open.ecrc.configuration.EcrcProperties;
-import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import ca.bc.gov.open.ecrc.configuration.EcrcProperties;
+import ca.bc.gov.open.ecrc.model.ValidationResponse;
+import ca.bc.gov.open.ecrc.service.ECRCJWTValidationServiceImpl;
+import ca.bc.gov.open.ecrc.util.AES256;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 public class JWTAuthorizationFilter  extends OncePerRequestFilter {
+	
+	private final Logger logger = LoggerFactory.getLogger(JWTAuthorizationFilter.class);
 
     private EcrcProperties ecrcProps;
+    
+	private ECRCJWTValidationServiceImpl tokenValidationServices;
 
-    public JWTAuthorizationFilter(EcrcProperties ecrcProps) {
+    public JWTAuthorizationFilter(EcrcProperties ecrcProps, ECRCJWTValidationServiceImpl tokenValidationServices) {
         this.ecrcProps = ecrcProps;
+        this.tokenValidationServices = tokenValidationServices;
     }
 
     @Override
@@ -29,10 +41,25 @@ public class JWTAuthorizationFilter  extends OncePerRequestFilter {
         try {
             if (checkJWTToken(request)) {
                 Claims claims = validateToken(request);
-                if (claims.get("authorities") != null) {
+                if ( claims.get("authorities") != null ) {
+                	if (claims.get("per") != null) {
+                		logger.debug("Found PER claim. Validating....");
+                		String accessToken = AES256.decrypt((String)claims.get("per")); 
+                		if ( accessToken != null ) {
+                			ValidationResponse resp = tokenValidationServices.validateBCSCAccessToken(accessToken);  
+                			if ( !resp.isValid() ) {
+                				logger.error("Failed to validate PER claim of received JWT token. Validation Response said : " + resp.getMessage());
+                				SecurityContextHolder.clearContext(); // fail
+                			} else {
+                				logger.debug("PER decypted and Access Token validated.");
+                			}
+                		} else {
+                			SecurityContextHolder.clearContext(); // fail
+                		}
+                	}
                     setUpSpringAuthentication(claims);
                 } else {
-                    SecurityContextHolder.clearContext();
+                    SecurityContextHolder.clearContext(); // fail
                 }
             }
             chain.doFilter(request, response);
@@ -67,5 +94,7 @@ public class JWTAuthorizationFilter  extends OncePerRequestFilter {
         String authenticationHeader = request.getHeader(ecrcProps.getJwtHeader());
         return !(authenticationHeader == null || !authenticationHeader.startsWith(ecrcProps.getJwtPrefix()));
     }
+    
+    
 
 }
