d2d9fb76169eec148d608dcd155be2a5d7fe7782
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-23 14:25:48 -0700

    fix tests

--- src/ecrc-frontend/src/components/page/consent/Consent.test.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.test.js
@@ -5,12 +5,16 @@ import {
   render,
   fireEvent,
   getAllByRole,
-  getByText
+  getByText,
+  wait
 } from "@testing-library/react";
 import { createMemoryHistory } from "history";
 import { generateJWTToken } from "../../../modules/AuthenticationHelper";
+import axios from "axios";
 
 import Consent from "./Consent";
+
+jest.mock("axios");
 
 describe("Consent Page Component", () => {
   window.scrollTo = jest.fn();
@@ -29,23 +33,26 @@ describe("Consent Page Component", () => {
     legalFirstNm: "Robert",
     legalSecondNm: "Norman",
     legalSurnameNm: "Ross",
+    alias1FirstNm: "Alias1firstnm",
+    alias1SecondNm: "Alias1secondnm",
+    alias1SurnameNm: "Alias1surnamenm",
+    alias2FirstNm: "Alias2firstnm",
+    alias2SecondNm: "Alias2secondnm",
+    alias2SurnameNm: "Alias2surnamenm",
+    alias3FirstNm: "Alias3firstnm",
+    alias3SecondNm: "Alias3secondnm",
+    alias3SurnameNm: "Alias3surnamenm",
     birthPlace: "Daytona Beach, Florida",
     birthDt: "1942-10-29",
     genderTxt: "Male",
     driversLicNo: "1234567",
     phoneNumber: "2501234567",
-    emailAddress: "bob.ross@example.com",
-    addressLine1: "123 Somewhere",
-    cityNm: "Here",
-    provinceNm: "British Columbia",
-    postalCodeTxt: "V9V 9V9",
     countryNm: "Canada",
     mailingAddressLine1: "456 Elsewhere",
     mailingCity: "There",
     mailingProvince: "Ontario",
     mailingPostalCode: "V1V 1A1",
-    jobTitle: "Painter",
-    organizationFacility: ""
+    jobTitle: "Painter"
   };
 
   const org = {
@@ -70,7 +77,7 @@ describe("Consent Page Component", () => {
     sessionStorage.setItem("validator", "secret");
     sessionStorage.setItem("uuid", "unique123");
     generateJWTToken({
-      actionsPerformed: ["userConfirmation"],
+      actionsPerformed: ["infoReview"],
       authorities: ["Authorized"]
     });
   });
@@ -92,30 +99,6 @@ describe("Consent Page Component", () => {
     expect(history.location.pathname).toEqual("/hosthome");
   });
 
-  test("Validate Continue Redirect", () => {
-    const history = createMemoryHistory();
-    const { container } = render(
-      <Router history={history}>
-        <Consent page={page} />
-      </Router>
-    );
-
-    expect(getByText(container, "Continue").disabled).toBeTruthy();
-
-    const checkbox = getAllByRole(container, "checkbox");
-
-    fireEvent.click(checkbox[0]);
-    fireEvent.click(checkbox[1]);
-    fireEvent.click(checkbox[2]);
-
-    expect(getByText(container, "Continue").disabled).toBeFalsy();
-
-    fireEvent.click(getByText(container, "Continue"));
-    expect(history.location.pathname).toEqual(
-      "/criminalrecordcheck/applicationform"
-    );
-  });
-
   test("Validate Redirect to Home when unauthorized", () => {
     const history = createMemoryHistory();
 
@@ -131,5 +114,126 @@ describe("Consent Page Component", () => {
     );
 
     expect(history.location.pathname).toEqual("/");
+  });
+
+  test("Validate Employee relationship flow", async () => {
+    const page = {
+      header,
+      applicant,
+      org,
+      setApplicationInfo,
+      saveApplicant,
+      saveOrg,
+      saveApplicationInfo,
+      setError
+    };
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          sessionId: "123",
+          invoiceId: "123",
+          serviceFeeAmount: "123"
+        }
+      })
+    );
+
+    axios.post.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          partyId: "123",
+          serviceId: "123",
+          urlResponse: "http://sample.com"
+        }
+      })
+    );
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Consent page={page} />
+      </Router>
+    );
+
+    const checkbox = getAllByRole(container, "checkbox");
+
+    fireEvent.click(checkbox[0]);
+    fireEvent.click(checkbox[1]);
+    fireEvent.click(checkbox[2]);
+    fireEvent.click(getByText(container, "Continue"));
+
+    await wait(() => {
+      expect(setApplicationInfo).toHaveBeenCalled();
+      expect(saveApplicant).toHaveBeenCalled();
+      expect(saveOrg).toHaveBeenCalled();
+      expect(saveApplicationInfo).toHaveBeenCalled();
+    });
+  });
+
+  test("Validate Volunteer relationship flow", async () => {
+    applicant.driversLicNo = "";
+    applicant.alias1FirstNm = "";
+    applicant.alias1SecondNm = "";
+    applicant.alias1SurnameNm = "";
+    applicant.alias2FirstNm = "";
+    applicant.alias2SecondNm = "";
+    applicant.alias2SurnameNm = "";
+    applicant.alias3FirstNm = "";
+    applicant.alias3SecondNm = "";
+    applicant.alias3SurnameNm = "";
+
+    applicant.organizationFacility = "";
+
+    org.orgApplicantRelationship = "VOLUNTEER";
+
+    const page = {
+      header,
+      applicant,
+      org,
+      setApplicationInfo,
+      saveApplicant,
+      saveOrg,
+      saveApplicationInfo,
+      setError
+    };
+
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          sessionId: "123",
+          invoiceId: "123",
+          serviceFeeAmount: "123"
+        }
+      })
+    );
+
+    axios.post.mockImplementation(() =>
+      Promise.resolve({
+        data: {
+          partyId: "123",
+          serviceId: "123",
+          urlResponse: "http://sample.com"
+        }
+      })
+    );
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Consent page={page} />
+      </Router>
+    );
+
+    const checkbox = getAllByRole(container, "checkbox");
+
+    fireEvent.click(checkbox[0]);
+    fireEvent.click(checkbox[1]);
+    fireEvent.click(checkbox[2]);
+    fireEvent.click(getByText(container, "Continue"));
+
+    await wait(() => {
+      expect(setApplicationInfo).toHaveBeenCalled();
+    });
+
+    expect(history.location.pathname).toEqual("/criminalrecordcheck/success");
   });
 });
