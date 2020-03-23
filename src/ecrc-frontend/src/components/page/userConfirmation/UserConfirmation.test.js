2692e405eb97438c153c2fab1da42a6cf23376b5
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-23 13:22:02 -0700

    Moved import

--- src/ecrc-frontend/src/components/page/userConfirmation/UserConfirmation.test.js
+++ src/ecrc-frontend/src/components/page/userConfirmation/UserConfirmation.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { create } from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 
 import UserConfirmation from "./UserConfirmation";
-import { MemoryRouter } from "react-router-dom";
 
 describe("User Confirmation Page Component", () => {
   test("Matches the snapshot", () => {
