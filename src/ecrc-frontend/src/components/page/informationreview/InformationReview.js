196d645de19f96018d4e464ded25338fb170458a
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-25 11:40:02 -0700

    fix linting

--- src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
+++ src/ecrc-frontend/src/components/page/informationreview/InformationReview.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-fragments */ // this is for sonarqube
 import React, { useState, useEffect } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
