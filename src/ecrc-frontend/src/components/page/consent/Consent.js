b8b2941f1e6a35c1d6400a6db22ec4733dd3697c
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-03-25 12:14:20 -0700

    break out promise

--- src/ecrc-frontend/src/components/page/consent/Consent.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.js
@@ -156,94 +156,91 @@ export default function Consent({
     let serviceFeeAmount;
     let serviceId;
 
-    Promise.all([
-      axios.post("/ecrc/private/createApplicant", createApplicantInfo, {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }),
-      axios.get(
-        `/ecrc/private/getNextSessionId?orgTicketNumber=${orgTicketNumber}&requestGuid=${uuid}`,
-        {
+    if (orgApplicantRelationship !== "VOLUNTEER") {
+      Promise.all([
+        axios.post("/ecrc/private/createApplicant", createApplicantInfo, {
           headers: {
             Authorization: `Bearer ${token}`
           }
-        }
-      ),
-      axios.get(
-        `/ecrc/private/getNextInvoiceId?orgTicketNumber=${orgTicketNumber}&requestGuid=${uuid}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
+        }),
+        axios.get(
+          `/ecrc/private/getNextSessionId?orgTicketNumber=${orgTicketNumber}&requestGuid=${uuid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           }
-        }
-      ),
-      axios.get(
-        `/ecrc/private/getServiceFeeAmount?orgTicketNumber=${orgTicketNumber}&scheduleTypeCd=${defaultScheduleTypeCd}&scopeLevelCd=${defaultCrcScopeLevelCd}&requestGuid=${uuid}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`
+        ),
+        axios.get(
+          `/ecrc/private/getNextInvoiceId?orgTicketNumber=${orgTicketNumber}&requestGuid=${uuid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           }
-        }
-      )
-    ])
-      .then(all => {
-        partyId = all[0].data.partyId;
-        sessionId = all[1].data.sessionId;
-        invoiceId = all[2].data.invoiceId;
-        serviceFeeAmount = all[3].data.serviceFeeAmount;
-
-        // NEED CLARIFICATION: - as per Jason Lee, awaiting confirmation
-        // eivPassDetailsResults - String returned from equifax, see Shaun
-        const newCRC = {
-          orgTicketNumber,
-          requestGuid: uuid,
-          schedule_Type_Cd: defaultScheduleTypeCd,
-          scope_Level_Cd: defaultCrcScopeLevelCd,
-          appl_Party_Id: partyId,
-          org_Appl_To_Pay: "A",
-          applicant_Posn: jobTitle,
-          child_Care_Fac_Nm: "child_Care_Fac_Nm",
-          governing_Body_Nm: "governing_Body_Nm",
-          session_Id: sessionId,
-          invoice_Id: invoiceId,
-          auth_Release_EIV_Vendor_YN: "Y",
-          auth_Conduct_CRC_Check_YN: "Y",
-          auth_Release_To_Org_YN: "Y",
-          appl_Identity_Verified_EIV_YN: "Y",
-          eivPassDetailsResults: "eivPassDetailsResults"
-        };
-
-        return axios.post("/ecrc/private/createNewCRCService", newCRC, {
-          headers: {
-            Authorization: `Bearer ${token}`
+        ),
+        axios.get(
+          `/ecrc/private/getServiceFeeAmount?orgTicketNumber=${orgTicketNumber}&scheduleTypeCd=${defaultScheduleTypeCd}&scopeLevelCd=${defaultCrcScopeLevelCd}&requestGuid=${uuid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
           }
-        });
-      })
-      .then(crcResponse => {
-        serviceId = crcResponse.data.serviceId;
+        )
+      ])
+        .then(all => {
+          partyId = all[0].data.partyId;
+          sessionId = all[1].data.sessionId;
+          invoiceId = all[2].data.invoiceId;
+          serviceFeeAmount = all[3].data.serviceFeeAmount;
 
-        const appInfo = {
-          partyId,
-          sessionId,
-          invoiceId,
-          serviceFeeAmount,
-          serviceId
-        };
+          // NEED CLARIFICATION: - as per Jason Lee, awaiting confirmation
+          // eivPassDetailsResults - String returned from equifax, see Shaun
+          const newCRC = {
+            orgTicketNumber,
+            requestGuid: uuid,
+            schedule_Type_Cd: defaultScheduleTypeCd,
+            scope_Level_Cd: defaultCrcScopeLevelCd,
+            appl_Party_Id: partyId,
+            org_Appl_To_Pay: "A",
+            applicant_Posn: jobTitle,
+            child_Care_Fac_Nm: "child_Care_Fac_Nm",
+            governing_Body_Nm: "governing_Body_Nm",
+            session_Id: sessionId,
+            invoice_Id: invoiceId,
+            auth_Release_EIV_Vendor_YN: "Y",
+            auth_Conduct_CRC_Check_YN: "Y",
+            auth_Release_To_Org_YN: "Y",
+            appl_Identity_Verified_EIV_YN: "Y",
+            eivPassDetailsResults: "eivPassDetailsResults"
+          };
 
-        setApplicationInfo(appInfo);
+          return axios.post("/ecrc/private/createNewCRCService", newCRC, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+        })
+        .then(crcResponse => {
+          serviceId = crcResponse.data.serviceId;
 
-        const currentPayload = accessJWTToken(sessionStorage.getItem("jwt"));
-        const newPayload = {
-          ...currentPayload,
-          actionsPerformed: [...currentPayload.actionsPerformed, "consent"]
-        };
-        generateJWTToken(newPayload);
+          const appInfo = {
+            partyId,
+            sessionId,
+            invoiceId,
+            serviceFeeAmount,
+            serviceId
+          };
 
-        if (orgApplicantRelationship === "VOLUNTEER") {
-          setLoading(false);
-          toSuccess();
-        } else {
+          setApplicationInfo(appInfo);
+
+          const currentPayload = accessJWTToken(sessionStorage.getItem("jwt"));
+          const newPayload = {
+            ...currentPayload,
+            actionsPerformed: [...currentPayload.actionsPerformed, "consent"]
+          };
+          generateJWTToken(newPayload);
+
           saveApplicant();
           saveOrg();
           saveApplicationInfo(appInfo);
@@ -269,11 +266,83 @@ export default function Consent({
               setLoading(false);
               window.location.href = urlResponse.data.paymentUrl;
             });
-        }
-      })
-      .catch(() => {
-        setLoading(false);
-      });
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    } else if (orgApplicantRelationship === "VOLUNTEER") {
+      Promise.all([
+        axios.post("/ecrc/private/createApplicant", createApplicantInfo, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }),
+        axios.get(
+          `/ecrc/private/getNextSessionId?orgTicketNumber=${orgTicketNumber}&requestGuid=${uuid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+      ])
+        .then(all => {
+          partyId = all[0].data.partyId;
+          sessionId = all[1].data.sessionId;
+
+          // NEED CLARIFICATION: - as per Jason Lee, awaiting confirmation
+          // eivPassDetailsResults - String returned from equifax, see Shaun
+          const newCRC = {
+            orgTicketNumber,
+            requestGuid: uuid,
+            schedule_Type_Cd: defaultScheduleTypeCd,
+            scope_Level_Cd: defaultCrcScopeLevelCd,
+            appl_Party_Id: partyId,
+            org_Appl_To_Pay: "A",
+            applicant_Posn: jobTitle,
+            child_Care_Fac_Nm: "child_Care_Fac_Nm",
+            governing_Body_Nm: "governing_Body_Nm",
+            session_Id: sessionId,
+            auth_Release_EIV_Vendor_YN: "Y",
+            auth_Conduct_CRC_Check_YN: "Y",
+            auth_Release_To_Org_YN: "Y",
+            appl_Identity_Verified_EIV_YN: "Y",
+            eivPassDetailsResults: "eivPassDetailsResults"
+          };
+
+          return axios.post("/ecrc/private/createNewCRCService", newCRC, {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
+        })
+        .then(crcResponse => {
+          serviceId = crcResponse.data.serviceId;
+
+          const appInfo = {
+            partyId,
+            sessionId,
+            invoiceId,
+            serviceFeeAmount,
+            serviceId
+          };
+
+          setApplicationInfo(appInfo);
+
+          const currentPayload = accessJWTToken(sessionStorage.getItem("jwt"));
+          const newPayload = {
+            ...currentPayload,
+            actionsPerformed: [...currentPayload.actionsPerformed, "consent"]
+          };
+          generateJWTToken(newPayload);
+
+          setLoading(false);
+          toSuccess();
+        })
+        .catch(() => {
+          setLoading(false);
+        });
+    }
   };
 
   if (toHome) {
