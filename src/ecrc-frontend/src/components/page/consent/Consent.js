9d50d815d5379a31eef7e070db4c33079423c7f5
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-03-24 17:31:26 -0700

    Loader fix

--- src/ecrc-frontend/src/components/page/consent/Consent.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.js
@@ -70,6 +70,7 @@ export default function Consent({
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    setLoading(false);
 
     if (!isAuthorized() || !isActionPerformed("infoReview")) {
       setToAppHome(true);
@@ -240,6 +241,7 @@ export default function Consent({
         generateJWTToken(newPayload);
 
         if (orgApplicantRelationship === "VOLUNTEER") {
+          setLoading(false);
           toSuccess();
         } else {
           saveApplicant();
@@ -264,11 +266,14 @@ export default function Consent({
               }
             })
             .then(urlResponse => {
+              setLoading(false);
               window.location.href = urlResponse.data.paymentUrl;
             });
         }
       })
-      .catch(() => {});
+      .catch(() => {
+        setLoading(false);
+      });
   };
 
   if (toHome) {
