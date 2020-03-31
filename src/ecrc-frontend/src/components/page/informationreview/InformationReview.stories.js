d17f68941e0355be99cf491f48bedb99c6147a53
Alan Dodge <alan.dodge@gov.bc.ca>
2020-03-31 15:12:08 -0700

    Fixed stories

--- src/ecrc-frontend/src/components/page/informationreview/InformationReview.stories.js
+++ src/ecrc-frontend/src/components/page/informationreview/InformationReview.stories.js
@@ -1,14 +1,9 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import { storiesOf } from "@storybook/react";
 
 import InformationReview from "./InformationReview";
-import {
-  generateJWTToken,
-  accessJWTToken
-} from "../../../modules/AuthenticationHelper";
+import { generateJWTToken } from "../../../modules/AuthenticationHelper";
 
 export default {
   title: "InformationReview",
@@ -48,79 +43,6 @@ const org = {
 
 const setError = () => {};
 const setShare = () => {};
-
-function LoadData(props) {
-  const header = {
-    name: "Criminal Record Check"
-  };
-
-  const applicant = {
-    legalFirstNm: "Robert",
-    legalSecondNm: "Norman",
-    legalSurnameNm: "Ross",
-    birthPlace: "Daytona Beach, Florida",
-    birthDt: "1942-10-29",
-    genderTxt: "Male",
-    driversLicNo: "1234567",
-    phoneNumber: "2501234567",
-    emailAddress: "bob.ross@example.com",
-    addressLine1: "123 Somewhere",
-    cityNm: "Here",
-    provinceNm: "British Columbia",
-    postalCodeTxt: "V9V 9V9",
-    countryNm: "Canada",
-    mailingAddressLine1: "456 Elsewhere",
-    mailingCity: "There",
-    mailingProvince: "Ontario",
-    mailingPostalCode: "V1V 1A1",
-    jobTitle: "Painter",
-    organizationFacility: ""
-  };
-
-  const org = {
-    orgNm: "New Org Name"
-  };
-
-  const setError = () => {};
-  const setShare = () => {};
-
-  const mock = new MockAdapter(axios);
-  const API_REQUEST = "/private/checkShare";
-
-  mock.onGet(API_REQUEST).reply(200, {
-    oldOrg: "Old Org Name",
-    oldCRCExpiration: "2021-10-01"
-  });
-
-  const page = {
-    header,
-    applicant,
-    org,
-    setError,
-    setShare
-  };
-
-  sessionStorage.setItem("validator", "secret");
-  sessionStorage.setItem("uuid", "unique123");
-
-  const currentPayload = accessJWTToken(sessionStorage.getItem("jwt"));
-  const newPayload = {
-    ...currentPayload,
-    actionsPerformed: [
-      "infoReview",
-      "appForm",
-      "tou",
-      "bcscRedirect",
-      "orgVerification",
-      "consent",
-      "userConfirmation"
-    ],
-    authorities: ["Authorized"]
-  };
-  generateJWTToken(newPayload);
-
-  return props.children({ page });
-}
 
 const page = {
   header,
@@ -164,16 +86,6 @@ export const ScheduleD = () => (
       }}
     />
   </MemoryRouter>
-);
-
-export const AvailableShare = () => (
-  <LoadData>
-    {data => (
-      <MemoryRouter>
-        <InformationReview page={data.page} />
-      </MemoryRouter>
-    )}
-  </LoadData>
 );
 
 storiesOf("Information Review", module)
