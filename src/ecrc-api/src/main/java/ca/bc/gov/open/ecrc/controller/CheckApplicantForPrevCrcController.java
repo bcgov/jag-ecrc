c55b9d7b283c643f322c2339deb227b4394dd69c
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-04-01 15:05:58 -0700

    Updated to get request

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/CheckApplicantForPrevCrcController.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/CheckApplicantForPrevCrcController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,7 +28,7 @@ public class CheckApplicantForPrevCrcController {
 
 	Logger logger = LoggerFactory.getLogger(CheckApplicantForPrevCrcController.class);
 
-	@PostMapping(value = "/private/checkApplicantForPrevCRC", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/checkApplicantForPrevCRC", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> checkApplicantForPrevCrc(@RequestBody RequestCheckApplicantForPrevCrc applicantInfo) {
 		logger.info("Create applicant request received {}", applicantInfo.getRequestGuid());
 
