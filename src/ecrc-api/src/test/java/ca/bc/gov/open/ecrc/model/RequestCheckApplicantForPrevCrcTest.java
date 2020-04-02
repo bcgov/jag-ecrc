f99183b0370535815819e9a2f02cfe431064a429
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-04-02 16:46:25 -0700

    Added tests for sharing api classes

---
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/model/RequestCheckApplicantForPrevCrcTest.java
@@ -0,0 +1,54 @@
+package ca.bc.gov.open.ecrc.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for check applicant for previous crc request bean
+ *  
+ * @author sivakaruna
+ *
+ */
+class RequestCheckApplicantForPrevCrcTest {
+
+	private final String expectedQueryString = "?OrgTicketNumber=orgTicketNumber"
+			+ "&Legal_Surname_Nm=legalSurnameNm"
+			+ "&Legal_First_Nm=legalFirstNm"
+			+ "&Birth_Dt=birthDt"
+			+ "&Gender_Txt=genderTxt"
+			+ "&Postal_Code_Txt=postalCodeTxt"
+			+ "&Drivers_Lic_No=driversLicNo"
+			+ "&Scope_Level_Cd=scopeLevelCd"
+			+ "&Previous_Service_Id=previousServiceId";
+
+	@DisplayName("Success - checkApplicantForPrevCrc request queryString")
+	@Test
+	public void generateQueryStringTest() {
+		RequestCheckApplicantForPrevCrc requestCheckApplicantForPrevCrc = new RequestCheckApplicantForPrevCrc();
+		requestCheckApplicantForPrevCrc.setBirthDt("birthDt");
+		requestCheckApplicantForPrevCrc.setDriversLicNo("driversLicNo");
+		requestCheckApplicantForPrevCrc.setGenderTxt("genderTxt");
+		requestCheckApplicantForPrevCrc.setLegalFirstNm("legalFirstNm");
+		requestCheckApplicantForPrevCrc.setLegalSurnameNm("legalSurnameNm");
+		requestCheckApplicantForPrevCrc.setPostalCodeTxt("postalCodeTxt");
+		requestCheckApplicantForPrevCrc.setOrgTicketNumber("orgTicketNumber");
+		requestCheckApplicantForPrevCrc.setPreviousServiceId("previousServiceId");
+		requestCheckApplicantForPrevCrc.setScopeLevelCd("scopeLevelCd");
+		requestCheckApplicantForPrevCrc.setRequestGuid("requestGuid");
+
+		Assertions.assertEquals("birthDt", requestCheckApplicantForPrevCrc.getBirthDt());
+		Assertions.assertEquals("driversLicNo", requestCheckApplicantForPrevCrc.getDriversLicNo());
+		Assertions.assertEquals("genderTxt", requestCheckApplicantForPrevCrc.getGenderTxt());
+		Assertions.assertEquals("legalFirstNm", requestCheckApplicantForPrevCrc.getLegalFirstNm());
+		Assertions.assertEquals("legalSurnameNm", requestCheckApplicantForPrevCrc.getLegalSurnameNm());
+		Assertions.assertEquals("postalCodeTxt", requestCheckApplicantForPrevCrc.getPostalCodeTxt());
+		Assertions.assertEquals("orgTicketNumber", requestCheckApplicantForPrevCrc.getOrgTicketNumber());
+		Assertions.assertEquals("previousServiceId", requestCheckApplicantForPrevCrc.getPreviousServiceId());
+		Assertions.assertEquals("scopeLevelCd", requestCheckApplicantForPrevCrc.getScopeLevelCd());
+		Assertions.assertEquals("requestGuid", requestCheckApplicantForPrevCrc.getRequestGuid());
+		
+		Assertions.assertEquals(expectedQueryString, requestCheckApplicantForPrevCrc.toQueryString());
+	}
+
+}
