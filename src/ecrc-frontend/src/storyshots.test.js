60a14af5529f178f917d3d6151c03803b823d7bf
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-04-01 17:15:36 -0700

    finalize changes

--- src/ecrc-frontend/src/storyshots.test.js
+++ src/ecrc-frontend/src/storyshots.test.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-console */
 import initStoryshots from "@storybook/addon-storyshots";
 
 window.scrollTo = jest.fn();
-console.error = jest.fn(); // for async storyshot errors, due to lack of support
 
 initStoryshots({
   /* configuration options */
