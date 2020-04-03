f07f0f7372caf8727ab183f43b654d58ff1bf351
Alan Dodge <alan.dodge@gov.bc.ca>
2020-04-03 11:02:47 -0700

    Fixed ONETIME to not continure with payment link generation

--- src/ecrc-frontend/src/components/page/consent/Consent.js
+++ src/ecrc-frontend/src/components/page/consent/Consent.js
@@ -315,6 +315,7 @@ export default function Consent({
           if (orgApplicantRelationship === "ONETIME") {
             toSuccess();
             setLoading(false);
+            return;
           }
 
           const createURL = {
