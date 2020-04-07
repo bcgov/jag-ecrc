613f874771775ea28b45890a833e6100137b1076
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-04-06 17:03:45 -0700

    add error page test cases

--- src/ecrc-frontend/src/components/page/error/Error.test.js
+++ src/ecrc-frontend/src/components/page/error/Error.test.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { create } from "react-test-renderer";
-import { MemoryRouter } from "react-router-dom";
+import { MemoryRouter, Router } from "react-router-dom";
+import {
+  render,
+  fireEvent,
+  getByText,
+  wait,
+  getByDisplayValue
+} from "@testing-library/react";
+import { createMemoryHistory } from "history";
 
 import Error from "./Error";
 
@@ -23,5 +31,128 @@ describe("Error Component", () => {
       </MemoryRouter>
     );
     expect(ErrorPage.toJSON()).toMatchSnapshot();
+  });
+
+  test("Validate home button click takes user home", async () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    fireEvent.click(getByText(container, "Home"));
+
+    await wait(() => {
+      expect(history.location.pathname).toEqual("/");
+      expect(sessionStorage.length).toEqual(0);
+    });
+  });
+
+  test("Validate service unavailable case", async () => {
+    console.error = jest.fn();
+
+    const error = null;
+
+    const page = {
+      header,
+      error
+    };
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    await expect(() => {
+      getByText(container, "wrongtext");
+    }).toThrow();
+
+    expect(
+      getByText(container, "Service is currently unavailable")
+    ).toBeTruthy();
+  });
+
+  test("Validate unauthorized 403 case", async () => {
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    await expect(() => {
+      getByText(container, "wrongtext");
+    }).toThrow();
+
+    expect(getByText(container, "Unauthorized entry")).toBeTruthy();
+  });
+
+  test("Validate login failed case", async () => {
+    const error = { status: 403, message: "BCSC login failed" };
+
+    const page = {
+      header,
+      error
+    };
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    await expect(() => {
+      getByText(container, "wrongtext");
+    }).toThrow();
+
+    expect(getByText(container, "Login failed")).toBeTruthy();
+  });
+
+  test("Validate session expired case", async () => {
+    const error = { ...error, status: 590 };
+
+    const page = {
+      header,
+      error
+    };
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    await expect(() => {
+      getByText(container, "wrongtext");
+    }).toThrow();
+
+    expect(getByText(container, "Your session has expired")).toBeTruthy();
+  });
+
+  test("Validate unexpected error case", async () => {
+    const error = { ...error, status: 204 };
+
+    const page = {
+      header,
+      error
+    };
+
+    const history = createMemoryHistory();
+    const { container } = render(
+      <Router history={history}>
+        <Error page={page} />
+      </Router>
+    );
+
+    await expect(() => {
+      getByText(container, "wrongtext");
+    }).toThrow();
+
+    expect(getByText(container, "An unknown error has occurred")).toBeTruthy();
   });
 });
