9d38a17193111e5fb4091f88e0dfa6a4dbe75149
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-04-15 14:48:46 -0700

    Added unit test for oidc config

---
+++ src/ecrc-api/src/test/java/ca/bc/gov/open/ecrc/service/OIDCConfigurationServiceTest.java
@@ -0,0 +1,44 @@
+package ca.bc.gov.open.ecrc.service;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import ca.bc.gov.open.ecrc.configuration.EcrcProperties;
+import ca.bc.gov.open.ecrc.exception.EcrcServiceException;
+
+/**
+ * Tests for oidc Configuration Service
+ * 
+ * @author sivakaruna
+ *
+ */
+class OIDCConfigurationServiceTest {
+
+	@InjectMocks
+	OIDCConfigurationService oidcConfigurationService = new OIDCConfigurationService();
+
+	@Mock
+	EcrcProperties ecrcProperties;
+
+	@BeforeEach
+	void initialize() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(ecrcProperties.getOauthWellKnown()).thenReturn("${uri}");
+		Mockito.when(ecrcProperties.getOauthBCSCTimeout()).thenReturn(60000);
+	}
+
+	//Cannot test success without using the real well-known url
+	@DisplayName("Failure - getConfig oidcConfiguration service")
+	@Test
+	void testGetConfig() throws EcrcServiceException {
+		Assertions.assertNull(oidcConfigurationService.getConfig());
+	}
+}
