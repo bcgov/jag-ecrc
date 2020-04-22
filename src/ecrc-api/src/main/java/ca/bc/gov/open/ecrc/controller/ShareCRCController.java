1d1308fe99537824fee11f8c6d95655d9eb54998
TayGov <Taylor.Clausen@gov.bc.ca>
2020-04-22 14:29:02 -0700

    Sharing CRC Implementation

--- src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/ShareCRCController.java
+++ src/ecrc-api/src/main/java/ca/bc/gov/open/ecrc/controller/ShareCRCController.java
@@ -2,28 +2,30 @@ package ca.bc.gov.open.ecrc.controller;
 
 import ca.bc.gov.open.ecrc.exception.EcrcExceptionConstants;
 import ca.bc.gov.open.ecrc.exception.WebServiceStatusCodes;
+import ca.bc.gov.open.ecrc.model.RequestCRCShare;
 import ca.bc.gov.open.ecrc.service.EcrcServices;
-import ca.bc.gov.open.ecrc.model.RequestNewCRCService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CreateNewCRCServiceController {
+public class ShareCRCController {
     @Autowired
     EcrcServices ecrcServices;
 
-    Logger logger = LoggerFactory.getLogger(CreateNewCRCServiceController.class);
+    Logger logger = LoggerFactory.getLogger(ShareCRCController.class);
 
-    @PostMapping(value = "/private/createNewCRCService", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> createNewCRCService(@RequestBody(required=true) RequestNewCRCService requestNewCRCService) {
-        logger.info("Create new crc request received [{}]", requestNewCRCService.getRequestGuid());
+    @PostMapping(value = "/private/shareCRCService", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> shareCRCService(@RequestBody(required=true) RequestCRCShare requestCRCShare) {
+        logger.info("Share crc request received [{}]", requestCRCShare.getRequestCreateApplicant().getRequestGuid());
         try {
-            return ecrcServices.createNewCRCService(requestNewCRCService);
+            return ecrcServices.createCRCShare(requestCRCShare);
         } catch (Exception ex) {
             logger.error("Error in ecrc service: ", ex);
             return new ResponseEntity<>(String.format(EcrcExceptionConstants.WEBSERVICE_ERROR_JSON_RESPONSE,
