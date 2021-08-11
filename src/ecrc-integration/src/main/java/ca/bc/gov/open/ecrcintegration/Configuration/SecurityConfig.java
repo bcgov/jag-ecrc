b12ffa94997f64d8faebc3da7a78fec88eacec96
Ethan <ethan.kwan@nttdata.com>
2021-08-11 10:03:54 -0700

    disable swagger prod and add error security exception

--- src/ecrc-integration/src/main/java/ca/bc/gov/open/ecrcintegration/Configuration/SecurityConfig.java
+++ src/ecrc-integration/src/main/java/ca/bc/gov/open/ecrcintegration/Configuration/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.authorizeRequests().antMatchers("/error").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
